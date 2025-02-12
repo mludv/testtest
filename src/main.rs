8db03ab2d3cbb92079f00f30af10c0fb25320032
Max Ludvigsson <max.ludvigsson@pulsar.com>
2025-02-12 14:42:28 +0800

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,11 @@
 // This is the best implementation of this program
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, world!");
+// a function which prints a message
+fn print(m: &str) {
+    println!("{m}");
 }
